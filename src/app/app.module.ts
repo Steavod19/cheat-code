5a0e07f149eb7001fd5e8329dc7cb807a1c78840
Steve Rogers <srogers.design@gmail.com>
2017-07-14 01:00:29 -0400

    added material flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import { CheatCodeComponent } from './components/cheat-code/cheat-code.component';
 import { LeftNavComponent } from './components/cheat-code/left-nav/left-nav.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CodeContentComponent } from './components/cheat-code/code-content/code-content.component';
 
 
 
@@ -20,17 +22,15 @@ import { FooterComponent } from './components/footer/footer.component';
     LeftNavComponent,
     TopNavComponent,
     FooterComponent,
+    CodeContentComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
     BrowserAnimationsModule,
-    MdButtonModule,
-    MdMenuModule,
-    MdCardModule,
-    MdToolbarModule,
-    MdIconModule
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
